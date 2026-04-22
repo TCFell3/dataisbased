26bab46331e4823240169f2424f4813c223bae3f
johndoe <johndoe@email.com>
2026-04-23 09:25:34 +1200

    save changes

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import sqlite3
 
-db = sqlite3.connect("treetable.db")
-cursor = db.cursor()
-sql = "SELECT * FROM thetreetable;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+DATABASE = "treetable.db"
+
+
+def print_all_trees():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM thetreetable;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for thetreetable in results:
+        print(f"{thetreetable[1]:<10}{thetreetable[2]:<10}{thetreetable[3]:<10}{thetreetable[4]:<10}")
+    db.close()
+
+
+print_all_trees()
